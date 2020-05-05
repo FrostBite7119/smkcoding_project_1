c74b3d14e6062cde3e8778a03c477a97891ac314
FrostBite7119 <handy7119@gmail.com>
2020-05-05 15:54:13 +0700

    Validasi input.

--- app/src/main/java/com/example/smkcoding_project1/MainActivity.kt
+++ app/src/main/java/com/example/smkcoding_project1/MainActivity.kt
@@ -3,19 +3,59 @@ package com.example.smkcoding_project1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*;
 
 class MainActivity : AppCompatActivity() {
+
+    private var namaInput : String = ""
+    private var emailInput : String = ""
+    private var telpInput : String = ""
+    private var alamatInput : String = ""
+    private var genderInput : String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnSave.setOnClickListener { goToProfilActivity() }
+        setDataSpinnerGender()
+        btnSave.setOnClickListener { validasiInput() }
     }
 
     private fun goToProfilActivity() {
         val intent = Intent(this, ProfilActivity::class.java)
         startActivity(intent)
+    }
+
+    private fun validasiInput() {
+        namaInput = edtName.text.toString()
+        emailInput = edtEmail.text.toString()
+        telpInput = edtTelp.text.toString()
+        alamatInput = edtAlamat.text.toString()
+        genderInput = spinnerGender.selectedItem.toString()
+
+        when{
+            namaInput.isEmpty() -> edtName.error = "Nama tidak boleh kosong!"
+            genderInput.equals("Pilih Jenis Kelamin", ignoreCase = true) -> tampilToast("Jenis kelamin harus dipilih!")
+            emailInput.isEmpty() -> edtEmail.error = "Email tidak boleh kosong!"
+            telpInput.isEmpty() -> edtTelp.error = "Telp tidak boleh kosong!"
+            alamatInput.isEmpty() -> edtAlamat.error = "Alamat tidak boleh kosong!"
+
+            else -> {
+                tampilToast("Navigasi ke halaman profil")
+                goToProfilActivity()
+            }
+        }
+    }
+
+    private fun tampilToast(s: String) {
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun setDataSpinnerGender(){
+        val adapter = ArrayAdapter.createFromResource(this, R.array.jenis_kelamin, android.R.layout.simple_spinner_dropdown_item)
+
+        spinnerGender.adapter = adapter
     }
 }
